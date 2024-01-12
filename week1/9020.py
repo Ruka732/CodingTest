c58e9b2f6f2aa1f76a81cc08814199f233fa447c
jumin <ruka732@gmail.com>
2024-01-12 17:51:37 +0900

    코어 끝

--- week1/9020.py
+++ week1/9020.py
@@ -1,23 +1,11 @@
 # 골드바흐의 추측
 
-import sys
-
-input = sys.stdin.readline
-
-n = int(input())
-
-for i in range(n):
-  a = int(input())
-  arr = []
-  
-  for p in range(2, a):
-    for q in range(2, p+1):
-      if p % q == 0:
-        if p == q:
-          arr.append(p)
-        break
-  
-  for i in arr:
-    for j in arr:
-      if i + j == a:
-        print(i, j)
+def isPrime(n):
+  if n == 1:
+    return False
+  else:
+    for i in range(2, int(n**(0.5) + 1)):
+      if n % i == 0:
+        return False
+      else: 
+        return True
---
+++ week1/lose9020.py
@@ -0,0 +1,25 @@
+# 골드바흐의 추측
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(n):
+  a = int(input())
+  arr = []
+  
+  for p in range(2, a):
+    for q in range(2, p+1):
+      if p % q == 0:
+        if p == q:
+          arr.append(p)
+        break
+  
+  for i in arr:
+    for j in arr:
+      if i + j == a:
+        if i > j:
+          break
+        print(i, j)
